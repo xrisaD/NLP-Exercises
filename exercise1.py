e007d19ad4a06ff48eedbdc4def10bb37a807102
xrisaD <xrdikonimaki@hotmail.com>
2020-03-29 23:59:59 +0300

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -11,7 +11,9 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from collections import Counter
 from nltk.util import ngrams
+from pprint import pprint
 
+#part1
 #downloads
 nltk.download('punkt')
 nltk.download('gutenberg')
@@ -37,9 +39,37 @@ for i, j , k in zip(training_set, development_set, test_set):
   training_s = whitespace_wt.tokenize(i)
   development_s = whitespace_wt.tokenize(j)
   test_s = whitespace_wt.tokenize(k)
+
   training_sentences.append(training_s)
   development_sentences.append(development_s)
   test_sentences.append(test_s)
   #save training words, use them later to create vocabulary
   for w in training_s:
     words.append(w)  
+
+#PorterStemmer:removing the commoner morphological and inflexional endings from words in English
+#also delete stopwords
+stemmer = PorterStemmer()
+training_stems = [stemmer.stem(w) for w in words if w not in stopwords.words('english')]
+
+#create vocabulary with words that occur more than 10 times
+count = nltk.FreqDist(training_stems)
+vocabulary = [w  for w in count if count[w]>10]
+#pprint(vocabulary)
+
+#replace other words with a special token *UNK*
+done_training_sentences = []
+for s in training_sentences:   
+  done_training_sent = [stemmer.stem(w) if (stemmer.stem(w) in vocabulary) else "*UNK*" for w in s]
+  done_training_sentences.append(done_training_sent)
+
+#Bigram and trigram language model for word sequences
+def createBiTrigrams(sentences):
+  bigram_counter = Counter()
+  trigram_counter = Counter()
+  for sent in sentences:
+    bigram_counter.update([gram for gram in ngrams(sent, 2, pad_left=True, pad_right=True,left_pad_symbol='<start>',right_pad_symbol='<end>') ])
+    trigram_counter.update([gram for gram in ngrams(sent, 3, pad_left=True, pad_right=True,left_pad_symbol='<start>',right_pad_symbol='<end>') ])
+  return (bigram_counter, trigram_counter)
+
+(bigram_counter, trigram_counter) = createBiTrigrams(done_training_sentences)
