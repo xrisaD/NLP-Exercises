7b0ee67ee3bb2426341fa585e5b8d9c7c4d87862
xrisaD <xrdikonimaki@hotmail.com>
2020-04-02 20:15:30 +0300

    Update love_psychologist.py
    love_psychologist chat

--- Book:NLPwithPython/love_psychologist.py
+++ Book:NLPwithPython/love_psychologist.py
@@ -1,3 +1,14 @@
+!pip install -U nltk
+from nltk.book import *
+from __future__ import division
+# text1.concordance("thing") :search for words
+# text1.similar("monstrous") :What other words appear in a similar range of contexts?
+# text1.common_contexts(["whale", "sea"]) :the contexts that are shared by two or more words
+# text1.dispersion_plot(["whale", "sea","Dick","Greece"]) : determine the location of a word in the text: how many words from the beginning it appears
+# sorted(set(text1))
+#text5.count("lol")
+#fdist1.hapaxes() :words that occur once only
+#bigrams(['more', 'is', 'said', 'than', 'done'])
 from nltk.chat.util import Chat, reflections
 pairs = [
     [r'(.*) happy because (.*)',
@@ -15,8 +26,15 @@ pairs = [
     [r'(.*) (like|love) (.*)',
     ["That's amazing! Tell me more!"]],
 
+    [r'I hate (.*)',
+    ["Relax..Let's discuss about it."]],
+
+    [r'(.*) hates (.*)',
+    ["All right! That seems important but %1 lose."]],
+
     [r'(.*)',
     ["%1. How do you feel about that?"]],
+
 ]
 def love_psychologist():
   print("Hi, I'm your psychologist. What's wrong? ") 
